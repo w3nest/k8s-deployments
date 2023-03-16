25dc64280bd96019e5568c8f9079274544fcf340
Julien Décharne <jdecharne@youwol.com>
2023-03-16 12:56:35 +0100

    🚀 setup OIDC cluster access

---
+++ vendor/ovh/scripts/setup_oidc.py
@@ -0,0 +1,23 @@
+import json
+import os
+
+import ovh
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = ovh.Client(
+    endpoint='ovh-eu',
+    application_key=os.getenv("APPLICATION_KEY"),
+    application_secret=os.getenv("APPLICATION_SECRET"),
+    consumer_key=os.getenv("CONSUMER_KEY"),
+)
+
+result = client.post(
+    f"/cloud/project/{os.getenv('SERVICE_NAME')}/kube/{os.getenv('KUBE_ID')}/openIdConnect",
+    issuerUrl="https://platform.int.youwol.com/auth/realms/kubernetes",
+    clientId="kubernetes-auth",
+    groupsClaim=["groups"],
+)
+
+print(json.dumps(result, indent=4))
